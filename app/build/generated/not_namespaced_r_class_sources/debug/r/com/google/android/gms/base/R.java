6d6bf140e11db9ed4abebbc0f2cb954b814c3e9b
Duy Le <banle@ucsc.edu>
2019-10-16 23:34:51 -0700

    implemented google login

--- app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
@@ -66,11 +66,11 @@ public final class R {
         public static final int adjust_width = 0x7f08003e;
         public static final int auto = 0x7f080043;
         public static final int dark = 0x7f08006e;
-        public static final int icon_only = 0x7f080092;
-        public static final int light = 0x7f08009e;
-        public static final int none = 0x7f0800af;
-        public static final int standard = 0x7f0800ed;
-        public static final int wide = 0x7f08011b;
+        public static final int icon_only = 0x7f080093;
+        public static final int light = 0x7f0800a0;
+        public static final int none = 0x7f0800b3;
+        public static final int standard = 0x7f0800f3;
+        public static final int wide = 0x7f08011f;
     }
     public static final class string {
         private string() {}
